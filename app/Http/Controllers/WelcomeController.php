176ee341e91abcb3a92e072a642bb13df9fa0630
SovyliaAdeleina <sovylia27@gmail.com>
2024-02-26 23:45:13 +0700

    alhamdulillah

--- app/Http/Controllers/WelcomeController.php
+++ app/Http/Controllers/WelcomeController.php
@@ -9,4 +9,14 @@ class WelcomeController extends Controller
 public function hello() {
     return 'Hello World';
 }
+// public function greeting(){
+//     return view('blog.hello', ['name' => 'Sovylia']);
+//    } 
+public function greeting(){
+ return view('blog.hello')
+ ->with('name','Sovylia')
+ ->with('occupation','Astronaut');
+ }
+
+
 }
